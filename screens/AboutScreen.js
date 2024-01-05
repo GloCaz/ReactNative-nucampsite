0826cdc980136c858e22744c322073495dd386ed
Adriana Cazarez <adriana.danzzer@gmail.com>
2024-01-05 05:38:08 -0700

    Activity Indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -2,13 +2,14 @@ import { ScrollView, Text } from 'react-native';
 import { Avatar, Card, ListItem } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from '../components/LoadingComponent';
 
-const Mission = () => {
+function Mission() {
     return (
         <Card>
             <Card.Title>Our Mission</Card.Title>
             <Card.Divider />
-            <Text style={{ margin: 10}}>
+            <Text style={{ margin: 10 }}>
             We present a curated database of the best 
             campsites in the vast woods and backcountry 
             of the World Wide Web Wilderness. We increase 
@@ -22,30 +23,58 @@ const Mission = () => {
             </Text>
         </Card>
     );
-};
+}
 
 const AboutScreen = () => {
     const partners = useSelector((state) => state.partners);
 
-    return (
-        <ScrollView>
-            <Mission />
+    if (partners.isLoading) {
+        return (
+            <ScrollView>
+                <Mission />
                 <Card>
                     <Card.Title>Community Partners</Card.Title>
                     <Card.Divider />
-                    {partners.partnersArray.map((partner) => (
-                        <ListItem key={partner.id}>
-                            <Avatar source={{ uri: baseUrl + partner.image }} rounded />
-                            <ListItem.Content>
-                                <ListItem.Title>{partner.name}</ListItem.Title>
-                                <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
-                            </ListItem.Content>
-                        </ListItem>
-                    ))}
+                    <Loading />
                 </Card>
+            </ScrollView>
+        );
+    }
+    if (partners.errMess) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    <Text>{partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+        );
+    }
+    return (
+        <ScrollView>
+            <Mission />
+            <Card>
+                <Card.Title>Community Partners</Card.Title>
+                <Card.Divider />
+                {partners.partnersArray.map((partner) => (
+                    <ListItem key={partner.id}>
+                        <Avatar
+                            rounded
+                            source={{ uri: baseUrl + partner.image }}
+                        />
+                        <ListItem.Content>
+                            <ListItem.Title>{partner.name}</ListItem.Title>
+                            <ListItem.Subtitle>
+                                {partner.description}
+                            </ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>
+                ))}
+            </Card>
         </ScrollView>
     );
 };
-
 
 export default AboutScreen;
