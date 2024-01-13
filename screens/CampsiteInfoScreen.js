207fd52b9fd97bf3f1984f3bdec8297257e12a6a
Adriana Cazarez <adriana.danzzer@gmail.com>
2024-01-12 18:35:22 -0700

    Swipe and Delete Favorites

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,4 +1,5 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Modal, FlatList, StyleSheet, Text, View } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import RenderCampsite from '../features/campsites/RenderCampsite';
 import { toggleFavorite } from '../features/favorites/favoritesSlice';
@@ -8,6 +9,27 @@ const CampsiteInfoScreen = ({ route }) => {
     const comments = useSelector((state) => state.comments);
     const favorites = useSelector((state) => state.favorites);
     const dispatch = useDispatch();
+
+    const [showModal, setShowModal] = useState(false);
+    const [rating, setRating] = useState(5);
+    const [author, setAuthor] = useState('');
+    const [text, setText] = useState('');
+
+    const handleSubmit = () => {
+        const newComment = {
+            author,
+            rating,
+            text,
+            campsiteId: campsite.id
+        };
+        console.log({newComment} = setShowModal(!showModal))
+    };
+
+    const resetForm = () => {
+            setRating(5);
+            setAuthor('');
+            setText('');
+        };
    
     const renderCommentItem = ({ item }) => {
         return (
@@ -22,29 +44,54 @@ const CampsiteInfoScreen = ({ route }) => {
     };
 
     return (
-        <FlatList
-            data={comments.commentsArray.filter(
-                (comment) => comment.campsiteId == campsite.id
-            )}
-            renderItem={renderCommentItem}
-            keyExtractor={(item) => item.id.toString()}
-            contentContainerStyle={{
-                marginHorizontal: 20,
-                paddingVertical: 20
-            }}
-            ListHeaderComponent={
-                <>
-                    <RenderCampsite 
-                        campsite={campsite} 
-                        isFavorite={favorites.includes(campsite.id)}
-                        markFavorite={() => 
-                            dispatch(toggleFavorite(campsite.id))
-                        }
-                    />
-                    <Text style={styles.commentsTitle}>Comment</Text>
-                </>
-            }
-        />
+        <>
+            <FlatList
+                data={comments.commentsArray.filter(
+                    (comment) => comment.campsiteId == campsite.id
+                )}
+                renderItem={renderCommentItem}
+                keyExtractor={(item) => item.id.toString()}
+                contentContainerStyle={{
+                    marginHorizontal: 20,
+                    paddingVertical: 20
+                }}
+                ListHeaderComponent={
+                    <>
+                        <RenderCampsite 
+                            campsite={campsite} 
+                            isFavorite={favorites.includes(campsite.id)}
+                            markFavorite={() => 
+                                dispatch(toggleFavorite(campsite.id))
+                            }
+                            onShowModal={() => setShowModal(!showModal)}
+                        />
+                        <Text style={styles.commentsTitle}>Comment</Text>
+                    </>
+                }
+            />
+            <Modal
+                animationType='slide'
+                transparent={false}
+                visible={showModal}
+                onRequestClose={() => setShowModal(!showModal)}
+            >
+                <View style={styles.modal}>
+                    <View style={{margin: 10}}>
+                        <Button 
+                            onPress={setShowModal(!showModal)}
+                            color={'#808080'}
+                            title='Cancel'>
+
+                        </Button>
+                    </View>
+                </View>
+                <Form>
+                    <Button>
+
+                    </Button>
+                </Form>
+            </Modal>
+        </>
     );
         
 };
@@ -63,7 +110,12 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
         paddingHorizontal: 20,
         backgroundColor: '#fff'
-    }
+    },
+    modal: {
+        justifyContent: 'center',
+        margin: 20
+    },
+
 });
 
 
